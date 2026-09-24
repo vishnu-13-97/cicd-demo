e81ef1213375bc5350dfe281e8ea2089663a2ad0
Vishnu <vishnu.cp.13@gmail.com>
2026-09-24 04:56:02 +0000

    fix application readiness test

--- test.sh
+++ test.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-response=$(curl -s http://localhost:8000)
+echo "Waiting for application..."
 
-if [ "$response" = "Hello from CI/CD - Version 2" ]; then
-    echo "TEST PASSED"
-    exit 0
-else
-    echo "TEST FAILED"
-    exit 1
-fi
+for i in {1..10}
+do
+    response=$(curl -s http://localhost:8000)
+
+    if [ "$response" = "Hello from CI/CD - Version 2" ]; then
+        echo "TEST PASSED"
+        exit 0
+    fi
+
+    sleep 2
+done
+
+echo "TEST FAILED"
+echo "Response: $response"
+
+exit 1
